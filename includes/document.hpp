2de16adfc2a9176fdc1f75e063a394d0f9490cee
hamzahassnaain <hamzahassanain067@gamil.com>
2025-08-17 09:17:01 +0300

    added docuemnt

---
+++ includes/document.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <element.hpp>
+namespace hamza_html_builder
+{
+
+    class document
+    {
+        std::shared_ptr<element> root;
+        std::string doctype;
+
+    public:
+        document(const std::string &doctype = "html")
+        {
+            root = std::make_shared<element>("html");
+        }
+        std::string to_string() const
+        {
+            std::string result = "<!DOCTYPE " + doctype + ">\n";
+            result += root->to_string();
+            return result;
+        }
+        ~document() = default;
+    };
+
+}
